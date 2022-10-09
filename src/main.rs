6623c01c729d084cd9f228cd809a5a9d4d1b6b56
Oto Brglez <otobrglez@gmail.com>
2022-10-09 22:10:57 +0200

    Refactoring...

--- src/main.rs
+++ src/main.rs
@@ -32,80 +32,96 @@ struct Episode {
     totalPlays: u32,
 }
 
+
 type CSRFToken = String;
 
-fn get_csrf_token(agent: &Agent) -> Result<CSRFToken, AnchorError> {
-    agent
-        .get(config::ANCHOR_CSRF_URL)
-        .call()
-        .map_err(to_anchor_error)
-        .and_then(parse_json)
-        .and_then(|json| {
-            json["csrfToken"]
-                .as_str()
-                .map(String::from)
-                .ok_or(AnchorError::NoCSRFToken)
-        })
+struct Anchor {
+    agent: Agent,
 }
 
-fn post_login(
-    agent: &Agent,
-    credentials: Credentials,
-    token: CSRFToken,
-) -> Result<(), AnchorError> {
-    agent
-        .post(config::ANCHOR_LOGIN_URL)
-        .send_json(ureq::json!({
+
+impl Anchor {
+    pub fn get_csrf_token(&mut self) -> Result<CSRFToken, AnchorError> {
+        self.agent
+            .get(config::ANCHOR_CSRF_URL)
+            .call()
+            .map_err(to_anchor_error)
+            .and_then(parse_json)
+            .and_then(|json| {
+                json["csrfToken"]
+                    .as_str()
+                    .map(String::from)
+                    .ok_or(AnchorError::NoCSRFToken)
+            })
+    }
+
+    pub fn post_login(&mut self, credentials: Credentials, token: CSRFToken) -> Result<(), AnchorError> {
+        self.agent
+            .post(config::ANCHOR_LOGIN_URL)
+            .send_json(ureq::json!({
             "email": credentials.email,
             "password": credentials.password,
             "_csrf": token,
         }))
-        .map_err(to_anchor_error)
-        .and_then(parse_string)
-        .map(|_| ())
-}
-
-fn get_metadata(agent: &Agent) -> Result<Metadata, AnchorError> {
-    agent
-        .get(config::ANCHOR_METADATA_URL)
-        .call()
-        .map_err(to_anchor_error)
-        .and_then(parse_json)
-        .map(|v| json_from_value(v).unwrap())
-}
-
-fn get_episodes(agent: &Agent, station_id: String) -> Result<JsonValue, AnchorError> {
-    agent
-        .get(&format!(anchor_episodes_url!(), station_id))
-        .call()
-        .map_err(to_anchor_error)
-        .and_then(parse_json)
-}
-
-fn all_episodes(agent: &Agent, station_id: String) -> Result<Vec<Episode>, AnchorError> {
-    fn transform_episodes(items: &Vec<JsonValue>) -> Vec<Episode> {
-        items
-            .iter()
-            .map(|i| json_from_value(i.clone()).unwrap())
-            .collect()
+            .map_err(to_anchor_error)
+            .and_then(parse_string)
+            .map(|_| ())
     }
 
-    get_episodes(agent, station_id).and_then(|json| {
-        json["items"]
-            .as_array()
-            .map(transform_episodes)
-            .ok_or(AnchorError::TransformationFailed)
-    })
+    pub fn get_metadata(&mut self) -> Result<Metadata, AnchorError> {
+        self.agent
+            .get(config::ANCHOR_METADATA_URL)
+            .call()
+            .map_err(to_anchor_error)
+            .and_then(parse_json)
+            .map(|v| json_from_value(v).unwrap())
+    }
+
+    fn get_episodes(&mut self, station_id: String) -> Result<JsonValue, AnchorError> {
+        self.agent
+            .get(&format!(anchor_episodes_url!(), station_id))
+            .call()
+            .map_err(to_anchor_error)
+            .and_then(parse_json)
+    }
+
+    pub fn all_episodes(&mut self, station_id: String) -> Result<Vec<Episode>, AnchorError> {
+        fn transform_episodes(items: &Vec<JsonValue>) -> Vec<Episode> {
+            items
+                .iter()
+                .map(|i| json_from_value(i.clone()).unwrap())
+                .collect()
+        }
+
+        self.get_episodes(station_id).and_then(|json| {
+            json["items"]
+                .as_array()
+                .map(transform_episodes)
+                .ok_or(AnchorError::TransformationFailed)
+        })
+    }
 }
 
 pub(crate) fn main() {
     let credentials = credentials::from_env();
 
     let agent: Agent = ureq::AgentBuilder::new()
-        .timeout_read(Duration::from_secs(5))
-        .timeout_write(Duration::from_secs(10))
+        .timeout_read(Duration::from_secs(2))
+        .timeout_write(Duration::from_secs(2))
         .build();
 
+    let mut anchor = Anchor { agent };
+    let _login = anchor.get_csrf_token()
+        .and_then(|token| anchor.post_login(credentials, token));
+
+    let episodes = anchor.get_metadata()
+        .and_then(|metadata| anchor.all_episodes(metadata.webStationId));
+
+    for Ok(episode) in episodes.iter() {
+        println!("{} {}", episode.title, episode.totalPlays);
+    }
+
+    /*
     let _token =
         get_csrf_token(agent.borrow()).and_then(|t| post_login(agent.borrow(), credentials, t));
 
@@ -119,4 +135,5 @@ pub(crate) fn main() {
             println!("{} {}", episode.title, episode.totalPlays);
         }
     });
+     */
 }
